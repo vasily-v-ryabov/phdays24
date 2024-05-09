4fef5349747be9452044bebd9189657624f71b9a
Vasily Ryabov <vasily.v.ryabov@gmail.com>
2024-05-09 22:43:38 +0300

    Generate MLIR from Python AST, add .clang-format with LLVM style

--- 02_MLIR_gen_pyvars/main.cpp
+++ 02_MLIR_gen_pyvars/main.cpp
@@ -11,31 +11,9 @@
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 
 #include "py_ast.h"
+#include "MLIRGen.h"
 
-
-mlir::ModuleOp mlirGen(mlir::MLIRContext &context, mod_ty py_module, const char *src_name) {
-  mlir::OpBuilder builder(&context);
-  context.getOrLoadDialect<mlir::LLVM::LLVMDialect>();
-
-  auto srcStringAttr = builder.getStringAttr(llvm::StringRef(src_name));
-  mlir::Location loc = mlir::FileLineColLoc::get(srcStringAttr, 0, 0);
-
-  auto module = mlir::ModuleOp::create(loc);
-  builder.setInsertionPointToEnd(module.getBody());
-
-  // create function main()
-  auto mainFuncType = mlir::LLVM::LLVMFunctionType::get(builder.getI32Type(), {});
-  auto mainFunc = builder.create<mlir::LLVM::LLVMFuncOp>(loc, "main", mainFuncType);
-  mlir::Block *entryBlock = mainFunc.addEntryBlock();
-  builder.setInsertionPointToStart(entryBlock);
-
-  
-
-  // return 0;
-  auto constOp = builder.create<mlir::LLVM::ConstantOp>(loc, builder.getI32Type(), 0);
-  builder.create<mlir::LLVM::ReturnOp>(loc, constOp->getResult(0));
-  return module;
-}
+static char *srcFilename = nullptr;
 
 int main(int argc, char **argv) {
   [[maybe_unused]] PythonRuntime runtime;
@@ -44,14 +22,18 @@ int main(int argc, char **argv) {
     std::cerr << "Usage: py39compiler <filename.py>\n";
     return 1;
   }
-  if (!script.parse_file(argv[1]))
+  srcFilename = argv[1];
+  if (!script.parse_file(srcFilename))
     return 2;
 
   mlir::MLIRContext context;
-  mlir::OwningOpRef<mlir::ModuleOp> module = mlirGen(context, script.mod(), argv[1]);
-  if (!module)
+  context.getOrLoadDialect<mlir::LLVM::LLVMDialect>();
+
+  MLIRGen gen(context, srcFilename);
+  if (mlir::failed(gen.mlirGen(script.mod())))
     return 3;
 
+  mlir::OwningOpRef<mlir::ModuleOp> module = gen.getModule();
   if (mlir::failed(mlir::verify(*module))) {
     module->emitError("Module verification failed!");
     module->dump(); // dump incorrect IR anyway
