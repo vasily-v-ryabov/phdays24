3eb025df64000053ad25e69172611bcabd521757
Vasily Ryabov <vasily.v.ryabov@gmail.com>
2024-05-11 15:47:57 +0300

    Remove redundant options from build scripts

--- 01_MLIR_gen/build.bat
+++ 01_MLIR_gen/build.bat
@@ -1,6 +1,6 @@
 ::"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-mkdir -p build
+mkdir build
 cd build
 cmake -S .. -B . -G "Visual Studio 17 2022" -DCMAKE_BUILD_TYPE=Debug
 cmake --build . --target py39compiler
