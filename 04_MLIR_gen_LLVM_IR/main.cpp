9ef9c917d43d63c80fcb6879c79944b0fd0ddcae
Vasily Ryabov <vasily.v.ryabov@gmail.com>
2024-05-19 10:36:52 +0300

    Add lowering to "llvm" dialect and translation to LLVM IR

--- 04_MLIR_gen_LLVM_IR/main.cpp
+++ 04_MLIR_gen_LLVM_IR/main.cpp
@@ -5,32 +5,60 @@
 //
 //===----------------------------------------------------------------------===//
 
+// for MLIRContext and OpBuilder with method verify()
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/IR/Verifier.h"
 
+// dialects
+#include "mlir/Dialect/ControlFlow/IR/ControlFlow.h"
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/Dialect/SCF/IR/SCF.h"
-#include "mlir/Dialect/ControlFlow/IR/ControlFlow.h"
 
-#include "mlir/Conversion/SCFToControlFlow/SCFToControlFlow.h"
+// for lowering to "llvm" dialect
 #include "mlir/Conversion/ControlFlowToLLVM/ControlFlowToLLVM.h"
+#include "mlir/Conversion/SCFToControlFlow/SCFToControlFlow.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Pass/PassManager.h"
 
-#include "py_ast.h"
+// for translation to LLVM IR
+#include "mlir/Target/LLVMIR/Dialect/Builtin/BuiltinToLLVMIRTranslation.h"
+#include "mlir/Target/LLVMIR/Dialect/LLVMIR/LLVMToLLVMIRTranslation.h"
+#include "mlir/Target/LLVMIR/Export.h"
+
+// for command line arguments parser
+#include "llvm/Support/CommandLine.h"
+#include <string>
+
 #include "MLIRGen.h"
+#include "py_ast.h"
 
 static char *srcFilename = nullptr;
 
+enum Action {
+  DumpMLIR,
+  DumpLLVM,
+  DumpLLVMIR,
+  RunJIT,
+};
+
+namespace cl = llvm::cl;
+static cl::opt<enum Action> emitAction(
+    "emit", cl::desc("Select the kind of output desired"),
+    cl::values(clEnumValN(DumpMLIR, "mlir", "output the MLIR dump")),
+    cl::values(clEnumValN(DumpLLVM, "llvm", "dump the MLIR \"llvm\" dialect")),
+    cl::values(clEnumValN(DumpLLVMIR, "llvm-ir", "output the LLVM IR dump")),
+    cl::values(clEnumValN(RunJIT, "jit", "JIT and run the main function")));
+
+static cl::opt<std::string> srcPy(cl::Positional, cl::desc("<input .py file>"),
+                                  cl::init("-"), cl::value_desc("filename"));
+
 int main(int argc, char **argv) {
+  cl::ParseCommandLineOptions(argc, argv, "Python 3.9 demo compiler\n");
+
+  // parse .py file
   [[maybe_unused]] PythonRuntime runtime;
   PyAST script;
-  if (argc <= 1) {
-    std::cerr << "Usage: py39compiler <filename.py>\n";
-    return 1;
-  }
-  srcFilename = argv[1];
-  if (!script.parse_file(srcFilename))
+  if (!script.parse_file(srcPy.c_str()))
     return 2;
 
   mlir::MLIRContext context;
@@ -49,12 +77,35 @@ int main(int argc, char **argv) {
     return 4;
   }
 
+  if (emitAction == Action::DumpMLIR) {
+    module->dump(); // dump MLIR to stderr
+    return 0;
+  }
+
+  // lower to "llvm" dialect
   mlir::PassManager passes(&context);
   passes.addPass(mlir::createConvertSCFToCFPass());
   passes.addPass(mlir::createConvertControlFlowToLLVMPass());
   if (mlir::failed(passes.run(module.get())))
     return 5;
 
-  module->dump(); // to stderr
-  return 0;
+  if (emitAction == Action::DumpLLVM) {
+    module->dump(); // dump MLIR to stderr
+    return 0;
+  }
+
+  // translate to LLVM IR
+  mlir::registerBuiltinDialectTranslation(*module->getContext());
+  mlir::registerLLVMDialectTranslation(*module->getContext());
+
+  llvm::LLVMContext llvmContext;
+  auto llvmModule = mlir::translateModuleToLLVMIR(*module, llvmContext);
+
+  if (emitAction == Action::DumpLLVMIR) {
+    llvm::errs() << *llvmModule << "\n"; // dump LLVM IR
+    return 0;
+  }
+
+  llvm::errs() << "Not supported -emit action!\n"; // JIT will be here
+  return 6;
 }
